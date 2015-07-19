4fc3cf0937ac5017a298368494b1a5e346eee9e5
plusCubed <plusCubed@users.noreply.github.com>
2015-07-19 12:04:03 +0800

    New icon, blue color theme

--- app/src/main/java/com/pluscubed/mishuzhushou/MainActivity.java
+++ app/src/main/java/com/pluscubed/mishuzhushou/MainActivity.java
@@ -1,7 +1,9 @@
 package com.pluscubed.mishuzhushou;
 
+import android.app.ActivityManager;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setTaskDescription(new ActivityManager.TaskDescription(null, null, getResources().getColor(R.color.primary_dark)));
+        }
 
         FragmentManager fragmentManager = getFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.activity_main_content_framelayout);
